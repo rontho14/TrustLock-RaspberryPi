a8d32c121f0edd2366e6ac65dc1a1989603eaa75
rontho14 <70487108+rontho14@users.noreply.github.com>
2025-06-03 17:54:32 -0300

    Adicionando buzzer e fechadura no main

--- main.py
+++ main.py
@@ -8,3 +8,23 @@ from RFID.MFRC522 import MFRC522
 from OLED.ssd1306 import SSD1306_I2C
 i2c = I2C(1, scl=Pin(3), sda=Pin(2), freq=400000)
 display = SSD1306_I2C(128, 32, i2c)
+
+def display_message(line1="", line2=""):
+    display.fill(0)
+    display.text(line1, 0, 0, 1)
+    display.text(line2, 0, 10, 1)
+    display.show()
+    
+# ---- BUZZER e FECHADURA ----
+buzzer = Pin(0, Pin.OUT)
+lock = Pin(22, Pin.OUT)
+def beep(times, duration_on=0.05, duration_off=0.05):
+    for _ in range(times):
+        buzzer.on()
+        utime.sleep(duration_on)
+        buzzer.off()
+        utime.sleep(duration_off)
+def open_lock(duration=5):
+    lock.on()
+    utime.sleep(duration)
+    lock.off()
